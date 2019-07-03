aa63e204e9fed30f806b9c21258d16bb3da36722
Travis Prescott <trpresco@microsoft.com>
2019-07-03 15:48:02 -0700

    Copy exclusions from Azure CLI.

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -5,8 +5,8 @@ set -ev
 echo "Install azdev into virtual environment"
 python -m venv env
 . env/bin/activate
-pip install -U pip setuptools wheel
-pip install $(find ${BUILD_ARTIFACTSTAGINGDIRECTORY}/pypi -name *.tar.gz)
+pip install -U pip setuptools wheel -q
+pip install $(find ${BUILD_ARTIFACTSTAGINGDIRECTORY}/pypi -name *.tar.gz) -q
 git clone https://github.com/Azure/azure-cli.git
 git clone https://github.com/Azure/azure-cli-extensions.git
 azdev setup -c -r azure-cli-extensions
