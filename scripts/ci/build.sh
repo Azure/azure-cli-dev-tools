99efd001f9dd01ad30974ecc2ff468a79b0c169a
Jianhui Harold <haroldrandom@gmail.com>
2019-11-19 12:05:14 +0800

    Fix bug which go to wrong dir and omit the generated wheel file

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -3,10 +3,11 @@
 set -ev
 
 : "${BUILD_STAGINGDIRECTORY:?BUILD_STAGINGDIRECTORY environment variable not set}"
-: "${BUILD_SOURCESDIRECTORY:="$(dirname ${BASH_SOURCE[0]})/../../.."}"
+: "${BUILD_SOURCESDIRECTORY:=$(cd $(dirname $0); cd ../../; pwd)}"
 
-cd $BUILD_SOURCESDIRECTORY
+cd "${BUILD_SOURCESDIRECTORY}"
 
 echo "Build azdev"
 pip install -U pip setuptools wheel
-python setup.py bdist_wheel sdist -d $BUILD_STAGINGDIRECTORY
+python setup.py bdist_wheel -d "${BUILD_STAGINGDIRECTORY}"
+python setup.py sdist -d "${BUILD_STAGINGDIRECTORY}"
