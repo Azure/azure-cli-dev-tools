1e19518496b4a745d1ae47a5f300c59088b9c3d5
MyronFanQiu <49134743+MyronFanQiu@users.noreply.github.com>
2020-01-19 11:50:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,10 +42,12 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     keywords='azure',
-    python_requires='>=2.7,!=3.4,!=3.3,!=3.2,!=3.1,!=3.0,<=3.8',
+    python_requires='>=2.7,!=3.4,!=3.3,!=3.2,!=3.1,!=3.0,<=3.9',
     packages=[
         'azdev',
         'azdev.config',
