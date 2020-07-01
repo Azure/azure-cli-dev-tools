217fe48f9f77b1bd8686719be41cd7c2e096e109
bquantump <53361486+bquantump@users.noreply.github.com>
2020-06-30 17:17:15 -0700

    WIP: fix install.sh

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -9,4 +9,4 @@ pip install -U pip setuptools wheel -q
 pip install $(find ${BUILD_ARTIFACTSTAGINGDIRECTORY}/pypi -name *.tar.gz) -q
 git clone https://github.com/Azure/azure-cli.git
 git clone https://github.com/Azure/azure-cli-extensions.git
-azdev setup -c -r azure-cli-extensions
+azdev setup -c azure-cli -r azure-cli-extensions
