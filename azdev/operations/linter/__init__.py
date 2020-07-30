91cf9b401aa21c2b440034676eb00217efad726c
MyronFanQiu <fanqiu@microsoft.com>
2020-07-30 10:56:04 +0800

    test coverage

--- azdev/operations/linter/__init__.py
+++ azdev/operations/linter/__init__.py
@@ -24,7 +24,7 @@ from .util import filter_modules, merge_exclusion
 
 logger = get_logger(__name__)
 CHECKERS_PATH = 'azdev.operations.linter.pylint_checkers'
-
+TEST_COMMANDS = ['C:\\','Codes','azure-cli-dev-folder','azure-cli','az_command_coverage.txt']
 
 # pylint:disable=too-many-locals, too-many-statements, too-many-branches
 def run_linter(modules=None, rule_types=None, rules=None, ci_exclusions=None,
@@ -186,3 +186,106 @@ def pylint_rules(selected_modules):
 
 def linter_severity_choices():
     return [str(severity.name).lower() for severity in LinterSeverity]
+
+
+def command_test_coverage(modules=None, rule_types=None, rules=None, ci_exclusions=None,
+                          git_source=None, git_target=None, git_repo=None, include_whl_extensions=False,
+                          min_severity=None, save_global_exclusion=False):
+    require_azure_cli()
+
+    from azure.cli.core import get_default_cli  # pylint: disable=import-error
+    from azure.cli.core.file_util import (  # pylint: disable=import-error
+        get_all_help, create_invoker_and_load_cmds_and_args)
+    # allow user to run only on CLI or extensions
+    cli_only = modules == ['CLI']
+    ext_only = modules == ['EXT']
+    if cli_only or ext_only:
+        modules = None
+
+    selected_modules = get_path_table(include_only=modules, include_whl_extensions=include_whl_extensions)
+
+    if cli_only:
+        selected_modules['ext'] = {}
+    if ext_only:
+        selected_modules['mod'] = {}
+        selected_modules['core'] = {}
+
+        # filter down to only modules that have changed based on git diff
+    selected_modules = filter_by_git_diff(selected_modules, git_source, git_target, git_repo)
+
+    if not any((selected_modules[x] for x in selected_modules)):
+        raise CLIError('No modules selected.')
+
+    selected_mod_names = list(selected_modules['mod'].keys()) + list(selected_modules['core'].keys()) + \
+                         list(selected_modules['ext'].keys())
+
+    if selected_mod_names:
+        display('Modules: {}\n'.format(', '.join(selected_mod_names)))
+
+    start = time.time()
+    display('Initializing linter with command table and help files...')
+    az_cli = get_default_cli()
+
+    # load commands, args, and help
+    create_invoker_and_load_cmds_and_args(az_cli)
+
+    command_loader = az_cli.invocation.commands_loader
+
+    # load yaml help
+    help_file_entries = {}
+
+    # trim command table and help to just selected_modules
+    command_loader, help_file_entries = filter_modules(
+        command_loader, help_file_entries, modules=selected_mod_names, include_whl_extensions=include_whl_extensions)
+
+    if not command_loader.command_table:
+        raise CLIError('No commands selected to check.')
+    simple_command_table = _format_command_table(command_loader.command_table)
+    parser = command_loader.cli_ctx.invocation.parser
+    path = os.path.join(*TEST_COMMANDS)
+    with open(path) as file:
+        import shlex
+        from azdev.operations.linter.rules.help_rules import _process_command_args
+        command = file.readline()
+        while command:
+            try:
+                command_args = shlex.split(command, comments=True)  # split commands into command args, ignore comments.
+                command_args, nested_commands = _process_command_args(command_args)
+                ns = parser.parse_args(command_args)
+                _update_command_table(simple_command_table, ns)
+            except:
+                print(command)
+                pass
+            command = file.readline()
+    _calculate_coverage_rate(simple_command_table)
+
+
+def _format_command_table(command_table):
+    simple_command_table = {}
+    ignore_arg = ['_subscription', 'cmd']
+    for command, value in command_table.items():
+        args_table = {arg: False for arg in value.arguments.keys() if arg not in ignore_arg}
+        simple_command_table[command] = args_table
+    return simple_command_table
+
+
+def _update_command_table(simple_command_table, namespace):
+    command = namespace.command
+    from knack.validators import DefaultInt, DefaultStr
+    if command in simple_command_table:
+        for key in simple_command_table[command].keys():
+            if hasattr(namespace, key) and getattr(namespace, key) and (type(getattr(namespace, key)) not in [DefaultInt, DefaultStr]):
+                simple_command_table[command][key] = True
+
+
+def _calculate_coverage_rate(simple_command_table):
+    command_coverage = {}
+    for command, args in simple_command_table.items():
+        command_group = command.split(' ')[0]
+        if command_group not in command_coverage:
+            command_coverage[command_group] = [0, 0]
+        command_coverage[command_group][1] += 1
+        if any(args.values()):
+            command_coverage[command_group][0] += 1
+    for command_group, value in command_coverage.items():
+        print("{} command coverage is {}".format(command_group, value[0]*1.0/value[1]))
