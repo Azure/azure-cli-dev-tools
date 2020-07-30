6eecd5bb415709ff3873b7b789fac176170606ae
Harold Zeng <haroldrandom@gmail.com>
2020-07-30 15:58:04 +0800

    pytest greater than 5.0.0

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
         'jinja2',
         'knack',
         'mock',
-        'pytest>=4.6.0',
+        'pytest>=5.0.0',
         'pytest-xdist', # depends on pytest-forked
         'pyyaml',
         'requests',
