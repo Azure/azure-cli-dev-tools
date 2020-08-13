35c12f230bf117a29f05386f2b4e9e71787e2970
MyronFanQiu <fanqiu@microsoft.com>
2020-08-13 14:56:55 +0800

    fix style

--- azdev/operations/test_coverage/__init__.py
+++ azdev/operations/test_coverage/__init__.py
@@ -147,7 +147,8 @@ def update_command_table(simple_command_table, namespace):
         for key in simple_command_table[command][0].keys():
             if all([hasattr(namespace, key),
                     getattr(namespace, key),
-                    type(getattr(namespace, key)) not in [DefaultInt, DefaultStr]]):
+                    type(getattr(namespace, key)) != DefaultInt,
+                    type(getattr(namespace, key)) != DefaultStr]):
                 simple_command_table[command][0][key] = True
 
 
