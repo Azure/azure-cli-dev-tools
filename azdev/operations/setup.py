d3835ab3de5ce667f376e9570d23b3164a063b10
Yalin Li <yall@microsoft.com>
2020-10-23 22:58:41 -0700

    Change print() to display()

--- azdev/operations/setup.py
+++ azdev/operations/setup.py
@@ -319,13 +319,13 @@ def setup(cli_path=None, ext_repo_path=None, ext=None, deps=None, set_env=None, 
         local_azdev_config = os.path.join(os.environ.get(const.VIRTUAL_ENV), '.azdev', const.CONFIG_NAME)
         cli_path = _get_azdev_cli_path(local_azdev_config)
         if cli_path is None:
-            print('Not found cli path in local azdev config file(' + local_azdev_config +
-            '), will use the one in global azdev config.')
+            display('Not found cli path in local azdev config file: ' + local_azdev_config)
+            display('Will use the one in global azdev config.')
             global_azdev_config = os.path.expanduser(os.path.join('~', '.azdev', const.CONFIG_NAME))
             cli_path = _get_azdev_cli_path(global_azdev_config)
             if cli_path is None:
                 raise CLIError('Not found cli path in global azdev config file: ' + global_azdev_config)
-        print('cli_path: ' + cli_path)
+        display('cli_path: ' + cli_path)
 
     _validate_input(cli_path, ext_repo_path, set_env, copy, use_global, ext)
     _check_paths(cli_path, ext_repo_path)
@@ -408,8 +408,7 @@ def _get_azdev_cli_path(config_file_path):
         content = config_parser._sections
         if 'cli' in content and 'repo_path' in content['cli']:
             return content.get('cli').get('repo_path')
-        else:
-            return None
+        return None
 
 
 def _handle_legacy(cli_path, ext_repo_path, ext, deps, start):
