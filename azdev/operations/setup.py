2a826a3f9e5e05b2e59fbc439abb72506fb3aa77
YalinLi0312 <yall@microsoft.com>
2020-11-09 13:12:09 -0800

    Move check_pyenv to check_env

--- azdev/operations/setup.py
+++ azdev/operations/setup.py
@@ -282,6 +282,7 @@ def _check_env(set_env):
     if not set_env:
         if not get_env_path():
             raise CLIError('You are not running in a virtual enviroment and have not chosen to set one up.')
+        _check_pyenv()
     elif 'VIRTUAL_ENV' in os.environ:
         raise CLIError("You are already running in a virtual enviroment, yet you want to set a new one up")
 
@@ -298,8 +299,6 @@ def _check_pyenv():
 
 def setup(cli_path=None, ext_repo_path=None, ext=None, deps=None, set_env=None, copy=None, use_global=None):
     _check_env(set_env)
-
-    _check_pyenv()
 
     _check_shell()
 
