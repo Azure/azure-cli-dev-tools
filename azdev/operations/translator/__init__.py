133b28b166b439e311c6fafae0a23f4e89c7332d
Kai Ru <kairu@microsoft.com>
2021-01-14 16:41:13 +0800

    add parse for argument

--- azdev/operations/translator/__init__.py
+++ azdev/operations/translator/__init__.py
@@ -7,6 +7,7 @@
 import importlib
 import os
 from collections import defaultdict
+import types
 
 from knack.cli import CLI
 from knack.commands import CLICommandsLoader
@@ -137,13 +138,17 @@ class AZDevTransCommand:
 
 
 # _argument_keys = set()
-# 'deprecate_info',
-# 'dest', 'help', 'option_list', 'options_list',
+# 'deprecate_info', 'is_preview', 'is_experimental',
+# 'dest', 'help', 'options_list',
 # 'action', 'arg_group', 'arg_type', 'choices', 'default',
 # 'completer', 'configured_default', 'const'
 # 'id_part',
-# 'is_preview', 'local_context_attribute', 'max_api', 'metavar', 'metave', 'min_api', 'nargs', 'operation_group',  'required', 'resource_type', 'type', 'validator']
+# 'local_context_attribute',
+# 'max_api', 'min_api',
+# 'nargs',  'required', 'type', 'validator'
 
+# ignored: 'metavar', 'operation_group', 'resource_type', 'dest'
+# invalid: 'option_list', 'metave', ' FilesCompleter'
 
 class AZDevTransArgument:
 
@@ -160,21 +165,29 @@ class AZDevTransArgument:
                     type_settings[key] = value
 
         self._parse_deprecate_info(type_settings)
+        self._parse_is_preview(type_settings)
+        self._parse_is_experimental(type_settings)
+        assert not (self.is_preview and self.is_experimental)
 
         self._parse_dest(type_settings)
         self._parse_help(type_settings)
-        self._parse_options_list(type_settings)  # TODO: FIX option_list code
-
-        self._parse_action(type_settings)   # TODO:
+        self._parse_options_list(type_settings)
         self._parse_arg_group(type_settings)
         self._parse_choices(type_settings)
         self._parse_default(type_settings)
-
-        self._parse_completer(type_settings)    # TODO:
-        self._parse_configured_default(type_settings)   # TODO: Don't know
-        self._parse_const(type_settings)    # TODO: Don't know
-
         self._parse_id_part(type_settings)
+        self._parse_max_api(type_settings)
+        self._parse_min_api(type_settings)
+        self._parse_nargs(type_settings)
+        self._parse_required(type_settings)
+        self._parse_configured_default(type_settings)
+        self._parse_const(type_settings)
+
+        self._parse_action(type_settings)   # TODO:
+        self._parse_completer(type_settings)    # TODO:
+        self._parse_local_context_attribute(type_settings)  # TODO:
+        self._parse_type(type_settings)         # TODO:
+        self._parse_validator(type_settings)    # TODO:
 
     def _parse_deprecate_info(self, type_settings):
         deprecate_info = type_settings.get('deprecate_info', None)
@@ -207,14 +220,6 @@ class AZDevTransArgument:
                 options_list = None
         self.options_list = options_list
 
-    def _parse_action(self, type_settings):
-        action = type_settings.get('action', None)
-        if action is not None:
-            if not isinstance(action, str):
-                # TODO: convert class type instance to string
-                pass
-        self.action = action
-
     def _parse_arg_group(self, type_settings):
         arg_group = type_settings.get('arg_group', None)
         if arg_group == '':
@@ -245,13 +250,6 @@ class AZDevTransArgument:
                     assert default in self.choices
         self.default = default
 
-    def _parse_completer(self, type_settings):
-        completer = type_settings.get('completer', None)
-        if completer is not None:
-            # TODO: convert completer to string
-            pass
-        self.completer = completer
-
     def _parse_configured_default(self, type_settings):
         configured_default = type_settings.get('configured_default', None)
         if configured_default == '':
@@ -265,6 +263,7 @@ class AZDevTransArgument:
             const = None
         if const is not None:
             assert self.action is not None and 'const' in self.action
+            assert isinstance(const, (str, int))
         self.const = const
 
     def _parse_id_part(self, type_settings):
@@ -274,24 +273,93 @@ class AZDevTransArgument:
         assert id_part is None or isinstance(id_part, str)
         self.id_part = id_part
 
-        # self.id_part = type_settings.get('id_part', None)
-        # self.options_list = type_settings.get('options_list', None)
-        # self.default = type_settings.get('default', None)
+    def _parse_is_preview(self, type_settings):
+        is_preview = type_settings.get('is_preview', False)
+        assert isinstance(is_preview, bool)
+        self.is_preview = is_preview
 
-        # self.required = type_settings.get('required', None)
-        # self.help_str = type_settings.get('help', None)
-        # self.action = type_settings.get('action', None)
-        # self.dest = type_settings.get('dest', None)
-        # assert self.name == self.dest
+    def _parse_is_experimental(self, type_settings):
+        is_experimental = type_settings.get('is_experimental', False)
+        assert isinstance(is_experimental, bool)
+        self.is_experimental = is_experimental
 
-#                   options_list=None, id_part=None, completer=None, validator=None, configured_default=None,
-#                  arg_group=None, arg_type=None, deprecate_info=None,
-#                  option_strings=None, dest=None,
-#                  nargs=None, const=None, default=None, type=None, choices=None, required=None, help=None, metavar=None,
-#                  action=None, default_value_source=None,
-#                  min_api=None, max_api=None, resource_type=None, operation_group=None, custom_command_type=None,
-#                  command_type=None, is_preview=None, preview_info=None, is_experimental=None, experimental_info=None,
-#                  local_context_attribute=None
+    def _parse_max_api(self, type_settings):
+        max_api = type_settings.get('max_api', None)
+        assert max_api is None or isinstance(max_api, str)
+        self.max_api = max_api
+
+    def _parse_min_api(self, type_settings):
+        min_api = type_settings.get('min_api', None)
+        assert min_api is None or isinstance(min_api, str)
+        self.min_api = min_api
+
+    def _parse_nargs(self, type_settings):
+        nargs = type_settings.get('nargs', None)
+        if nargs == '':
+            nargs = None
+        assert nargs is None or isinstance(nargs, int) or nargs in ['?', '*', '+']
+        self.nargs = nargs
+
+    def _parse_required(self, type_settings):
+        required = type_settings.get('required', False)
+        assert isinstance(required, bool)
+        self.required = required
+
+    def _parse_action(self, type_settings):
+        action = type_settings.get('action', None)
+        if action is not None:
+            if not isinstance(action, str):
+                # TODO: convert class type instance to string
+                pass
+        self.action = action
+
+    def _parse_completer(self, type_settings):
+        completer = type_settings.get('completer', None)
+        if completer is not None:
+            # TODO: convert completer to string
+            pass
+        self.completer = completer
+
+    def _parse_local_context_attribute(self, type_settings):
+        local_context_attribute = type_settings.get('local_context_attribute', None)
+        if local_context_attribute is not None:
+            # TODO: convert to string
+            pass
+        self.local_context_attribute = local_context_attribute
+
+    def _parse_type(self, type_settings):
+        typ = type_settings.get('type', str)
+        if typ not in (str, int, float, bool):
+            if isinstance(typ, types.FunctionType):
+                # TODO: convert to string
+                pass
+            elif isinstance(typ, types.MethodDescriptorType):
+                # print(typ)
+                # TODO: convert class method to string
+                pass
+            elif isinstance(typ, type):
+                # print(typ)
+                # TODO: convert class to string
+                pass
+            else:
+                # SizeWithUnitConverter
+                # TODO: convert callable instance to string
+                # print(type(typ), typ)
+                pass
+        self.typ = typ
+
+    def _parse_validator(self, type_settings):
+        validator = type_settings.get('validator', None)
+        if validator is not None:
+            assert isinstance(validator, types.FunctionType)
+            # TODO: Convert to string
+        self.validator = validator
+
+
+# option_strings=None,
+# default_value_source=None,
+# custom_command_type=None,
+# command_type=None,
 
 
 class AZDevTransModuleParser(CLICommandsLoader):
