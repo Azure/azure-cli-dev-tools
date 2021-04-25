6de82db34c2d2e049bdd98f194b1782e7d9c3adc
kai ru <69238381+kairu-ms@users.noreply.github.com>
2021-04-25 16:15:14 +0800

    detect both version.py and _version.py

--- azdev/operations/code_gen.py
+++ azdev/operations/code_gen.py
@@ -124,7 +124,10 @@ def _copy_vendored_sdk(src_path, dest_path):
     import shutil
 
     try:
-        client_location = find_files(src_path, '_version.py')[0]
+        version_files = find_files(src_path, 'version.py')
+        if not version_files:
+            version_files = find_files(src_path, '_version.py')
+        client_location = version_files[0]
     except IndexError:
         raise CLIError('Unable to find client files.')
 
