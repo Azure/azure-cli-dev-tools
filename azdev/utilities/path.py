2d5ca2aa9ad835749f26b6bf5588482c1fe48c12
ZelinWang <zelinwang@microsoft.com>
2023-12-01 17:31:12 +0800

    Update path.py

--- azdev/utilities/path.py
+++ azdev/utilities/path.py
@@ -241,8 +241,10 @@ def get_path_table(include_only=None, include_whl_extensions=False):
                     table[key].pop(short_name, None)
                     table[key][long_name] = folder
 
-    # Since include_only.remove will delete the found name, adjust the order of _update_table to ensure that extension is updated first.
-    # When the extension name and module name are the same, let azdev style tests the extension instead of the main module.
+    # Since include_only.remove will delete the found name
+    # Adjust the order of _update_table to ensure that extension is updated first.
+    # When the extension name and module name are the same
+    # Let azdev style tests the extension instead of the main module.
     _update_table(ext_paths, 'ext')
     if include_whl_extensions:
         _update_table(whl_ext_paths, 'ext')
