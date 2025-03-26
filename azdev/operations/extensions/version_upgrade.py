9616435f47e37982554975bd31e98b2697d48b67
AllyW <liwang1025@gmail.com>
2025-03-26 11:26:06 +0800

    add comment

--- azdev/operations/extensions/version_upgrade.py
+++ azdev/operations/extensions/version_upgrade.py
@@ -61,6 +61,9 @@ class VersionUpgradeMod:
         self.has_preview_tag = is_preview
         self.has_exp_tag = is_experimental
         # x.x.x + (isPreview/isExperimental: True)
+        # the versioning patter for previous preview pattern is as following:
+        # 1) 1.0.4 + isPreview: true -> stable: increase minor num to 1.1.0 + no preview tag
+        # 2) 1.0.4 + isPreview: true -> preview: increase patch num to 1.0.5b1 + preview tag
         self.is_preview_deprecate_pattern = bool((is_preview or is_experimental) and not self.version.pre)
         self.version_raw = current_version
         self.norm_versions()
