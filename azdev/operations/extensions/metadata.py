dc7d04a1883a2d76c08d59e16debc346bad1132a
ZelinWang <zelinwang@microsoft.com>
2025-04-07 11:41:41 +0800

    Add license headers

--- azdev/operations/extensions/metadata.py
+++ azdev/operations/extensions/metadata.py
@@ -1,5 +1,11 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -----------------------------------------------------------------------------
+
 """
-Tools for converting old- to new-style metadata.
+Tools for generate metadata for index.json.
 """
 
 import email.parser
