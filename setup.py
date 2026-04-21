9b0be21c96155600dba0b6074765eba4b511fce3
necusjz <necusjz@gmail.com>
2026-04-21 17:00:16 +1000

    feat: support python 3.14 (#544)

--- setup.py
+++ setup.py
@@ -40,14 +40,14 @@ setup(
         'Environment :: Console',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13'
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14'
     ],
     keywords='azure',
-    python_requires='>=3.9',
+    python_requires='>=3.10',
     packages=[
         'azdev',
         'azdev.config',
